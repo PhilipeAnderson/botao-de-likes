37d8ad59038df50ad9594225104bdfc1e2334978
Philipe Anderson De Campos <philipeacampos@hotmail.com>
2020-04-11 23:33:45 -0300

    Modificado os botoes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,37 @@
-numeroLikes=0
-function adicionarLike(){
-  numeroLikes=1+numeroLikes
-  if(numeroLikes)
-    {botao.innerHTML=numeroLikes+' Likes'}
-   if(numeroLikes==1)
-    {botao.innerHTML='1 Like'}
+numeroLikesFacebook = 0
+numeroLikesPinterest = 0
+numeroLikesYoutube = 0
+numeroLikesInstagram = 0
+
+
+function adicionarfacebook(){
+  numeroLikesFacebook++
+  if(numeroLikesFacebook)
+    {botao1.innerHTML = numeroLikesFacebook + ' Likes'}
+   if(numeroLikesFacebook == 1)
+    {botao1.innerHTML = '1 Like'}
   }
+
+  function adicionarpinterest(){
+    numeroLikesPinterest++
+    if(numeroLikesPinterest)
+      {botao2.innerHTML = numeroLikesPinterest + ' Likes'}
+     if(numeroLikesPinterest==1)
+      {botao2.innerHTML='1 Like'}
+    }
+
+    function adicionarYoutube(){
+      numeroLikesYoutube++
+      if(numeroLikesYoutube)
+        {botao3.innerHTML = numeroLikesYoutube + ' Likes'}
+       if(numeroLikesYoutube==1)
+        {botao3.innerHTML='1 Like'}
+      }
+
+      function adicionarInstagram(){
+        numeroLikesInstagram++
+        if(numeroLikesInstagram)
+          {botao4.innerHTML = numeroLikesInstagram + ' Likes'}
+         if(numeroLikesInstagram==1)
+          {botao4.innerHTML='1 Like'}
+        }
